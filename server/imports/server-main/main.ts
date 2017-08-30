1c3115f77739ac05b207f9517618d983b6e15a3f
Mehdi Karamnejad <me@mehdi.tech>
2017-08-30 01:04:35 -0700

    Adds basic graphql functionality

--- server/imports/server-main/main.ts
+++ server/imports/server-main/main.ts
@@ -1,4 +1,21 @@
+import {createApolloServer} from 'meteor/apollo';
+import {makeExecutableSchema} from 'graphql-tools';
+import {typeDefs} from '../graphql/schema';
+import {resolvers} from '../graphql/resolvers';
+
+
 export class Main {
-  start(): void {
-  }
+    private initApolloServer(): void {
+        const schema = makeExecutableSchema({
+            typeDefs,
+            resolvers,
+        });
+        createApolloServer({
+            schema,
+        });
+    }
+
+    start(): void {
+        this.initApolloServer();
+    }
 }
