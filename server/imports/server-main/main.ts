f14372d645d08a1b6f5ae86feb9fd768537fab72
Mehdi Karamnejad <me@mehdi.tech>
2017-09-04 11:55:33 -0700

    adds logging mechanism

--- server/imports/server-main/main.ts
+++ server/imports/server-main/main.ts
@@ -3,7 +3,6 @@ import {makeExecutableSchema} from 'graphql-tools';
 import {typeDefs} from '../graphql/schema';
 import {resolvers} from '../graphql/resolvers';
 
-
 export class Main {
     private initApolloServer(): void {
         const schema = makeExecutableSchema({
